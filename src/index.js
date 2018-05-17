10b44440ea8f2ea74b8214adfb61693d0945ff1a
Heather Christman <efl7a@yahoo.de>
2018-05-17 13:10:09 -0400

    made some changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 // import React from 'react' // We need to import react so we can make use of its .component class
 // import ReactDOM from 'react-dom' // ...and we need to import ReactDOM so we can create and test a virtual DOM with react!
+import React from 'react'
+import ReactDOM from 'react-dom'
 
 const BUTCHER_PRODUCTS = [
   'Tenderloin',
@@ -10,22 +12,41 @@ const BUTCHER_PRODUCTS = [
 
 // Define these exported classes
 export class OlderCoaster extends React.Component {
-  
+
   // this function should return the JSX we want the DOM to render
-  // render() {
-  //   return (
-  //     <div>Hello!</div>
-  //   )
-  // }
-  // 
+  render() {
+    return (
+      <div className="oldercoaster">
+        Two grannies having the time of their life!
+        Passengers:
+        {Agnes}
+        {Muriel}
+      </div>
+    )
+  }
+
 }
 
 export class InFrontOfYou extends React.Component {
-  // your code here
+  render() {
+    return (
+      <div>
+        You shouldn't look too far.
+        Sometimes, the solution is right in from of you.
+      </div>
+    )
+  }
 }
 
 export class ButcherShop extends React.Component {
-  // your code here
+  render() {
+    return (
+      <div>
+        Hello! We have the following products for sale today:
+        {BUTCHER_PRODUCTS}
+      </div>
+    )
+  }
 }
 
 
